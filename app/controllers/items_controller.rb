f91389570f4abdcf33531d550d87862d64a09af2
rieco-y <90105763+rieco-y@users.noreply.github.com>
2021-10-01 15:27:05 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_cost
   belongs_to :todouhuken
   belongs_to :send_schedule
-
-  be_longs_to :user
+  has_one attached :image
 end
