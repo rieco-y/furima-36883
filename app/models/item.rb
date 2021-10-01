c466fa2e01ff006493f6435ec19093329aa16a37
rieco-y <90105763+rieco-y@users.noreply.github.com>
2021-10-01 15:55:17 +0900

    Itemモデル　バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,20 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :item_name, presence: true
+  validates :item_text, presence: true
+  validates :price, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_cost_id, presence: true
+  validates :todouhuken_id, presence: true
+  validates :send_schedule_id, presence: true
+
+
   belongs_to :user
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_cost
   belongs_to :todouhuken
   belongs_to :send_schedule
-  has_one attached :image
+ # has_one attached :image
 end
