2485819089a00f1836025512d44cd44c95666ce7
rieco-y <90105763+rieco-y@users.noreply.github.com>
2021-10-03 15:23:54 +0900

    販売手数料、販売利益の表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,13 @@ class Item < ApplicationRecord
   
   validates :item_name, presence: true
   validates :item_text, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 , message: "is out of setting range"}, format: { with: /\A[0-9]+\z/ }
   validates :category_id, presence: true, numericality: { other_than: 1 , message: "Please select one"} 
   validates :condition_id, presence: true, numericality: { other_than: 1, message: "Please select one" } 
   validates :delivery_cost_id, presence: true, numericality: { other_than: 1 , message: "Please select one"} 
   validates :todouhuken_id, presence: true, numericality: { other_than: 1, message: "Please select one" } 
   validates :send_schedule_id, presence: true, numericality: { other_than: 1 , message: "Please select one"} 
+  validates :image, presence: true
 
   belongs_to :user
   belongs_to :category
@@ -19,3 +20,4 @@ class Item < ApplicationRecord
   has_one_attached :image
 
 end
+
