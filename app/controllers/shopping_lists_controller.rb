bce64391344de1f445d8411db149f7a7ad5938ba
rieco-y <90105763+rieco-y@users.noreply.github.com>
2021-10-07 16:42:07 +0900

    購入機能（shopping_list　controller）の設定

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,6 +1,25 @@
 class ShoppingListsController < ApplicationController
 
   def index
+    @shopping_list_address_list = ShoppingListAddressList.new(shopping_list_address_list_params)
+  end
+
+  def create
+    
+    @shopping_list_address_list = ShoppingListAddressList.new(shopping_list_address_list_params)
+    if @shopping_list_address_list.valid?
+      @shopping_list_address_list.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+  private
+
+  def shopping_list_address_list_params  
+      params.require(:shopping_list_address_list).permit(:post_num, :todouhuken_id, :city, :house_num, :building_name, :tel_num,).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
-    resources :shopping_lists, only: [:index, :new, :create]
+    resources :shopping_lists, only: [:index, :create]
   end
 end
 
