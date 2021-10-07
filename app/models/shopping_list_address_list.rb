464fec0a8b8f01dd2d8e00128d223b772f184dc9
rieco-y <90105763+rieco-y@users.noreply.github.com>
2021-10-07 14:24:30 +0900

    建物名のカラム名修正

--- app/models/shopping_list_address_list.rb
+++ app/models/shopping_list_address_list.rb
@@ -1,7 +1,7 @@
 class ShoppingListAddressList
 
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_num, :todouhuken_id, :city, :house_num, :building_nam, :tel_num, :shopping_list_id
+  attr_accessor :user_id, :item_id, :post_num, :todouhuken_id, :city, :house_num, :building_name, :tel_num, :shopping_list_id
 
   with_options presence: true do
     validates :user_id
@@ -16,7 +16,7 @@ class ShoppingListAddressList
 
   def save
     shopping_list = ShoppingList.create(user_id: user_id, item_id: item_id)
-    AddressList.create(post_num: post_num, todouhuken_id: todouhuken_id, city: city, house_num: house_num, building_nam: building_nam, tel_num: tel_num, shopping_list_id: shopping_list.id)
+    AddressList.create(post_num: post_num, todouhuken_id: todouhuken_id, city: city, house_num: house_num, building_name: building_name, tel_num: tel_num, shopping_list_id: shopping_list.id)
   end
 
 end
