b20efa836cea32c99523c2cbd3637a7bc238fea4
rieco-y <90105763+rieco-y@users.noreply.github.com>
2021-10-08 17:55:16 +0900

    フォームオブジェクトの設定

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,12 +1,13 @@
 class ShoppingListsController < ApplicationController
 
   def index
-    @shopping_list_address_list = ShoppingListAddressList.new(shopping_list_address_list_params)
+    @shopping_list_address_list = ShoppingListAddressList.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
-    
-    @shopping_list_address_list = ShoppingListAddressList.new(shopping_list_address_list_params)
+    @item = Item.find(params[:item_id])
+    @shopping_list_address_list = ShoppingListAddressList.new(shopping_list_params)
     if @shopping_list_address_list.valid?
       @shopping_list_address_list.save
       redirect_to root_path
@@ -18,8 +19,8 @@ class ShoppingListsController < ApplicationController
 
   private
 
-  def shopping_list_address_list_params  
-      params.require(:shopping_list_address_list).permit(:post_num, :todouhuken_id, :city, :house_num, :building_name, :tel_num,).merge(user_id: current_user.id, item_id: params[:item_id])
+  def shopping_list_params
+    params.permit(:hoge, :user_id, :item_id, :post_num, :todouhuken_id, :city, :house_num, :building_name, :tel_num, :shopping_list_id).merge(user_id: current_user.id)
   end
 
 end
--- app/models/shopping_list_address_list.rb
+++ app/models/shopping_list_address_list.rb
@@ -1,18 +1,17 @@
 class ShoppingListAddressList
 
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_num, :todouhuken_id, :city, :house_num, :building_name, :tel_num, :shopping_list_id
+  attr_accessor :user_id, :item_id, :post_num, :todouhuken_id, :city, :house_num, :building_name, :tel_num, :shopping_list_id, :hoge
 
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :shopping_list_id
     validates :post_num, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :todouhuken_id, numericality: {other_than: 1, message: 'Please select one'}
     validates :city
     validates :house_num
     validates :tel_num, format: {with: /\A\d{10,11}\z/, message: "is invalid"}
   end
-  validates :todouhuken_id, numericality: {other_than: 0, message: 'Please select one'}
 
   def save
     shopping_list = ShoppingList.create(user_id: user_id, item_id: item_id)
