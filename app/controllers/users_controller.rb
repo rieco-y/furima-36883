f91a07eb839e1a00394462a829220793c63a75aa
rieco-y <90105763+rieco-y@users.noreply.github.com>
2021-10-12 23:05:24 +0900

    usersコントコーラの記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
 
   def edit
+    @user = User.find(params[:id])
   end
   
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.valid?
+      bypass_sign_in(@user) 
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password)
+  end
 end
