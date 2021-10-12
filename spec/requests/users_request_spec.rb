075be3c85a0a58d08e521f70babcd4df2b3e16cc
rieco-y <90105763+rieco-y@users.noreply.github.com>
2021-10-13 02:21:58 +0900

    usersコントローラの単体テスト

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -2,4 +2,32 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
 
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'edit' do
+    it 'editアクションにリクエストすると正常にレスポンスが返ってくる' do 
+      @user.save
+      get edit_user_path(@user)
+      expect(response.status).to eq 200
+    end
+    it 'editアクションにリクエストするとレスポンスに新しいニックネームの入力フォームが存在する' do 
+      @user.save
+      get edit_user_path(@user)
+      expect(response.body).to include("新しいニックネーム")
+    end
+    it 'editアクションにリクエストするとレスポンスに新しいメールアドレスの入力フォームが存在する' do 
+      @user.save
+      get edit_user_path(@user)
+      expect(response.body).to include("新しいメールアドレス")
+    end
+    it 'editアクションにリクエストするとレスポンスに新しいパスワードの入力フォームが存在する' do 
+      @user.save
+      get edit_user_path(@user)
+      expect(response.body).to include("新しいパスワード")
+    end
+  end 
+
+
 end
